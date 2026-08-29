c9c78a9e62addd1c17f204671c6d60c37cf68ace
Avril Enriquez Vilca <aenriquezvilca@gmail.com>
2026-08-28 21:56:33 -0500

    Crear proyecto Java y StudentController

---
+++ src/lab/pkg4c24/grp/pkg03/Lab4C24GRP03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg4c24.grp.pkg03;
+
+/**
+ *
+ * @author usuario
+ */
+public class Lab4C24GRP03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
